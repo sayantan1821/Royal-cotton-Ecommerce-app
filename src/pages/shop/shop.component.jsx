a94f0a031bfefa4b0f5ef85ef436bdd082a59285
Sayantan Kapat <ksayantan1823@gmail.com>
2021-05-08 21:07:26 +0530

    shop data is connected with the database

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,15 +4,18 @@ import { Route } from 'react-router-dom';
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { updateCollections } from '../../redux/shop/shop.actions';
 
 class ShopPage extends React.Component{
   unsubscribeFromSnapshot = null;
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
     collectionRef.onSnapshot(async snapshot => {
       const collectionMap = convertCollectionsSnapshotToMap(snapshot);
-      console.log(collectionMap);
-    })
+      updateCollections(collectionMap);
+    });
   }
   render() {
     const { match } = this.props;
@@ -24,5 +27,8 @@ class ShopPage extends React.Component{
     );
   }
 } 
-
-export default ShopPage;
+const mapDispatchToProps = dispatch => ({
+  updateCollections : collectionMap => 
+    dispatch(updateCollections(collectionMap))
+})
+export default connect(null, mapDispatchToProps)(ShopPage);
