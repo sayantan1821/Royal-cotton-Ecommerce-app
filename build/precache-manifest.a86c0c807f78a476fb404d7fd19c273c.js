68c35535cb7a38fe089107b73947b506fb0a7551
Sayantan Kapat <ksayantan1823@gmail.com>
2021-05-10 12:55:51 +0530

    dployng in netlify

---
+++ build/precache-manifest.a86c0c807f78a476fb404d7fd19c273c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85fa9730d5e8b9844a200a32c17e107a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d787ae8e03a1e77d1d59",
+    "url": "/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "87fec0e061af377ddfef",
+    "url": "/static/js/2.27f2e71a.chunk.js"
+  },
+  {
+    "revision": "d787ae8e03a1e77d1d59",
+    "url": "/static/js/main.8f19aa41.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "d531d33f40acce2bb69e5b05b39ad67e",
+    "url": "/static/media/crown.d531d33f.svg"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "/static/media/shopping-bag.30f07071.svg"
+  }
+]);
