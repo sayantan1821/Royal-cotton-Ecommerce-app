1f3367ea9df3896ef01925ccf6843e0983eaa9b2
Sayantan Kapat <ksayantan1823@gmail.com>
2021-05-10 11:48:51 +0530

    updating icons

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -22,7 +22,7 @@ export const HeaderContainer = styled.div`
 export const LogoContainer = styled(Link)`
   height: 100%;
   width: 70px;
-  padding: 25px;
+  padding: 0px 5px;
 
   @media screen and (max-width: 800px) {
     width: 50px;
